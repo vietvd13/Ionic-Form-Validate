5a3d861f3011ff53c3ff1a5e71a05937f79f2d0a
Vũ Đức Việt <42991887+vietvd13@users.noreply.github.com>
2021-11-15 22:53:13 +0700

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -34,7 +34,7 @@ function convertDate(date: string) {
   const newDate = new Date(date);
   const year = newDate.getFullYear();
   const month = (newDate.getMonth() + 1);
-  const day = newDate.getDay();
+  const day = newDate.getDate();
   
 
   return joinDate(year, month, day);
